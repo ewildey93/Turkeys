5f70a312c2a98a53feef218f244ac102e1568f04
Wildey <wildeefb@DNR.state.wi.us>
2025-10-14 17:14:43 -0500

    Added bit to compare tagged turkeys in photos to those with a .csv with locations from transmitters

--- LocationsfromCameras.R
+++ LocationsfromCameras.R
@@ -6,6 +6,11 @@ ImageData <- read.csv("Z:/Sandhill/Turkey Project/Photos/TrailCams2025/CsvExport
 ImageData2 <- ImageData%>%filter(TagID != "")
 ImageData2[grep(x = ImageData2, pattern = "^[A-Z]{1}\\d{3}$")]
 
+TurkeyLocFiles <- list.files(path = "Z:/Sandhill/Turkey Project/Transmitter Files/Deployed Transmitter GPS Data Downloads", 
+                             pattern = "all_locations", recursive = TRUE)
+TelemTurkeys <- str_extract(string = TurkeyLocFiles, pattern = "([A-Za-z]+\\d{3})_.*_.*$", group = 1)
+TelemTurkeys <- gsub(pattern = "[a-z]*", replacement = "", x = TelemTurkeys)
+
 #left_join keeps all the records in X
 Image.DeploymentData <- left_join(x = ImageData2, y = DeploymentData, by = "Deployment")
 
@@ -14,6 +19,7 @@ Image.DeploymentData2 <-separate_longer_delim(Image.DeploymentData, TagID, delim
 Image.DeploymentData2 <- Image.DeploymentData2[grep(x = Image.DeploymentData2$TagID, pattern = "^[A-Z]{1}\\d{3}$"),]
 colstokeep <- c("DateTime", "TagID", "Latitude", "Longitude")
 Image.DeploymentData3 <- Image.DeploymentData2[,colstokeep]
+CompareTurkeyIDs <- data.frame("ID"=unique(Image.DeploymentData3$TagID), "HasTelemCSV"=unique(Image.DeploymentData3$TagID) %in% TelemTurkeys)
 
 #split dataframe into different IDs
 Image.DeploymentDataList <- split(Image.DeploymentData3, f = Image.DeploymentData2$TagID)
