d5c7cd0de21b4352515a68151c91d9e96806936a
Wildey <wildeefb@DNR.state.wi.us>
2025-10-15 13:13:06 -0500

    Finished writing/commenting code, should be ready to go

--- LocationsfromCameras.R
+++ LocationsfromCameras.R
@@ -1,28 +1,48 @@
 library(dplyr)
 library(tidyr)
 
+#read in Deployment Data csv
 DeploymentData <- read.csv("Z:/Sandhill/Turkey Project/Photos/TrailCams2025/CsvExportedFiles/ExportedDataDDBDate10-14/Deployment.csv")
+#read in Image Data csv
 ImageData <- read.csv("Z:/Sandhill/Turkey Project/Photos/TrailCams2025/CsvExportedFiles/ExportedDataDDBDate10-14/ImageData.csv")
+#filter out image data with no tagged turkeys
 ImageData2 <- ImageData%>%filter(TagID != "")
-ImageData2[grep(x = ImageData2, pattern = "^[A-Z]{1}\\d{3}$")]
 
-TurkeyLocFiles <- list.files(path = "Z:/Sandhill/Turkey Project/Transmitter Files/Deployed Transmitter GPS Data Downloads", 
+#get path for telemetry files
+path <- "Z:/Sandhill/Turkey Project/Transmitter Files/Deployed Transmitter GPS Data Downloads"
+#list all_location.xlsx filenames for each bird pulled from subfolders
+TurkeyLocFiles <- list.files(path = path, 
                              pattern = "all_locations", recursive = TRUE)
-TelemTurkeys <- str_extract(string = TurkeyLocFiles, pattern = "([A-Za-z]+\\d{3})_.*_.*$", group = 1)
-TelemTurkeys <- gsub(pattern = "[a-z]*", replacement = "", x = TelemTurkeys)
+#make data frame of all_location.xlsx filesnames, and turkey ID extracted from filenames, then arrange alphabeticcaly by ID
+TelemTurkeys <- data.frame("fp"=TurkeyLocFiles, "ID"=str_extract(string = TurkeyLocFiles, pattern = "([A-Za-z]+\\d{3})_.*_.*$", group = 1))%>%
+                arrange(ID)
+#Shorten turkey ID removing lower case letters to get B123 format
+TelemTurkeys$ID <- gsub(pattern = "[a-z]*", replacement = "", x = TelemTurkeys$ID)
+#add column extracting folders contain telemetry all_location.xlsx from file path, this will be important for writing .csvs of camera locations
+TelemTurkeys$dir <- paste(path,paste0(dirname(TelemTurkeys$fp), "/"), sep="/")
 
-#left_join keeps all the records in X
+#left_join keeps all the records in X, add deployment data(lat/lon) to photo data
 Image.DeploymentData <- left_join(x = ImageData2, y = DeploymentData, by = "Deployment")
 
-#separate rows with multiple tagged birds into separate lines
+#separate rows (photos) with multiple tagged birds into separate lines
 Image.DeploymentData2 <-separate_longer_delim(Image.DeploymentData, TagID, delim = ",") #14725
+#filter out uncertain turkey tags
 Image.DeploymentData2 <- Image.DeploymentData2[grep(x = Image.DeploymentData2$TagID, pattern = "^[A-Z]{1}\\d{3}$"),]
+#make a vector of what columns to keep
 colstokeep <- c("DateTime", "TagID", "Latitude", "Longitude")
+#shorten data frame to just datetime, tagid, and lat/lon
 Image.DeploymentData3 <- Image.DeploymentData2[,colstokeep]
-CompareTurkeyIDs <- data.frame("ID"=unique(Image.DeploymentData3$TagID), "HasTelemCSV"=unique(Image.DeploymentData3$TagID) %in% TelemTurkeys)
+#ass column to telem turkeys data frame to see which turkeys with telemetry have been tagged in photos
+TelemTurkeys$HasTelemCSV <- TelemTurkeys$ID %in% unique(Image.DeploymentData3$TagID)
+
 
 #split dataframe into different IDs
 Image.DeploymentDataList <- split(Image.DeploymentData3, f = Image.DeploymentData2$TagID)
-filepath <- "Z:/Sandhill/Turkey Project/Transmitter Files/Deployed Transmitter GPS Data Downloads/"
-filenames <- paste0(filepath,names(Image.DeploymentDataList), "CameraLocs.csv")
-mapply(function (x,y) write.csv(x,y),x=Image.DeploymentDataList, y=filenames)
+#shorten list of tagged photos to just those Turkey IDs that have a telemetry spreadsheet
+Image.DeploymentDataList2 <- Image.DeploymentDataList[names(Image.DeploymentDataList) %in% TelemTurkeys$ID]
+#get file path for all_locations.xlsx and match to photos of tag birds for export
+filepath <- TelemTurkeys$dir[which(TelemTurkeys$ID %in% names(Image.DeploymentDataList2))]
+#make filenames (including full path) for .csvs we will export for each tagged birds photo locations
+filenames <- paste0(filepath,names(Image.DeploymentDataList2), "CameraLocs.csv")
+#export .csvs of each tagged birds photo locations
+mapply(function (x,y) write.csv(x,y),x=Image.DeploymentDataList2, y=filenames)
